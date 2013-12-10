b3adac926aa949b24a951eb7428a5942f2912e2e
Zaki Manian <zaki@manian.org>
2013-12-09 21:59:11 -0800

    System for diplaying signatures. Still haven't figure out a way to construct a grid well. Want to display only a subset of signatures

---
+++ js/visuals.js
@@ -0,0 +1,46 @@
+
+window.viz ={};
+window.viz.SampleSingatureData =[{obfuscated: true, firstLen:4, lastLen:6, zip: 94065},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022} ];
+
+
+window.viz.updateSignatureCount = function(){
+  $('#sig_number').text(viz.SampleSingatureData.length).fadeIn("slow"); 
+};
+
+window.viz.displaySignature = function(sigdata){
+  if (sigdata.obfuscated === undefined){
+      return;
+  }
+  else if (sigdata.obfuscated === true){
+    var obfuscationString = "REDACTED"
+    while (obfuscationString.length <= sigdata.firstLen || obfuscationString <= sigdata.lastLen ){obfuscationString +=obfuscationString;}
+    var sigcontainer = $('<div>',{id:"sigcontainer",class:"sigcontainer"});
+    var firstname = $('<div>',{id:"firstname",class:"redacted"}).text(obfuscationString.substring(0,sigdata.firstLen));
+    var br = $('<br>',{id:"break",class:""});
+    var lastname = $('<div>',{id:"lastname",class:"redacted"}).text(obfuscationString.substring(0,sigdata.lastLen));
+    var zip = $('<div>',{id:"zip"}).text(sigdata.zip);
+    var sigcontents = sigcontainer.append(firstname).append(br).append(lastname).append(zip);
+    return $('#signhead').append(sigcontents).fadeIn("slow");
+  }
+  else if (sigdata.obfuscated === false){
+    var sigcontainer = $('<div>',{id:"sigcontainer",class:"sigcontainer"});
+    var firstname = $('<div>',{id:"firstname",class:""}).text(sigdata.firstName);
+    var lastname = $('<div>',{id:"lastname",class:""}).text(sigdata.lastName);
+    var zip = $('<div>',{id:"zip"}).text(sigdata.zip);
+    var sigcontents =sigcontainer.append(firstname).append(lastname).append(zip);
+    return $('#signhead').append(sigcontents).fadeIn("slow");
+  }
+};
+
+window.viz.orderSignatures = function(){
+    var column = $('<div>',{id:"subcol",class:"col-md-2"});
+    for (var i = 0;i<viz.SampleSingatureData.length;i++){
+      viz.displaySignature(viz.SampleSingatureData[i]);
+      }
+}
+
+$( document ).ready( function(){
+  viz.updateSignatureCount();
+  viz.orderSignatures();
+  
+} );
