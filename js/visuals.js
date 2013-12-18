dc8550ca160498096f6f5f61872ec7e808ba25f0
Zaki Manian <zaki@manian.org>
2013-12-17 20:56:27 -0800

    Working on vis for the signatures

--- js/visuals.js
+++ js/visuals.js
@@ -3,12 +3,9 @@ window.viz ={};
 window.viz.signature_count = 0;
 window.viz.signature_count_redacted = 0;
 
-window.viz.SampleSingatureData =[{obfuscated: true, firstLen:4, lastLen:6, zip: 94065},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated: true, firstLen:4, lastLen:6, zip: 94065},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated: true, firstLen:4, lastLen:6, zip: 94065},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated:false, firstName: "Zaki", lastName:"Manian", zip :94022},{obfuscated: true, firstLen:4, lastLen:6, zip: 94065}  ];
-
-
 
 window.viz.getSignatures = function(){
-  $.ajax({
+  return $.ajax({
     url:"/signatures",
     dataType:'json',
   }).success(function(data){
@@ -20,7 +17,7 @@ window.viz.getSignatures = function(){
 }
 
 window.viz.getSingaturesRedacted = function(){
-  $.ajax({
+  return $.ajax({
     url:"/signatures_redacted",
     dataType:'json',
   }).success(function(data){
@@ -38,29 +35,47 @@ window.viz.updateSignatureCount = function(total_count){
   $('#sig_number_2').text(total_count).fadeIn("slow"); 
 };
 
+window.viz.isObfuscatedSig = function(sigdata){
+  if (sigdata.firstLen === undefined){
+    return false;
+  }
+  if (sigdata.first === undefined){
+    return true;
+  }
+};
+
+viz.sigDateCompare = function (a,b){
+
+  if (Date.parse(a.date) < Date.parse(b.date)){ return -1;}
+  if (Date.parse(a.date) > Date.parse(b.date)){ return 1;}
+  return 0;
+}
+
+
+window.viz.displayData = function(){
+      viz.updateSignatureCount(window.viz.signature_count + window.viz.signature_count_redacted);
+      viz.orderSignatures();
+}
 
 
 
 window.viz.displaySignature = function(sigdata){
-  if (sigdata.obfuscated === undefined){
-      return;
-  }
-  else if (sigdata.obfuscated === true){
+  if (viz.isObfuscated(sigdata)){
     var obfuscationString = "REDACTED"
     while (obfuscationString.length <= sigdata.firstLen || obfuscationString <= sigdata.lastLen ){obfuscationString +=obfuscationString;}
     var sigcontainer = $('<div>',{id:"sigcontainer",class:"sigcontainer"});
     var firstname = $('<div>',{id:"firstname",class:"redacted"}).text(obfuscationString.substring(0,sigdata.firstLen));
     var br = $('<br>',{id:"break",class:""});
     var lastname = $('<div>',{id:"lastname",class:"redacted"}).text(obfuscationString.substring(0,sigdata.lastLen));
-    var zip = $('<div>',{id:"zip"}).text(sigdata.zip);
+    var zip = $('<div>',{id:"zip"}).text(sigdata.zip_code);
     var sigcontents = sigcontainer.append(firstname).append(br).append(lastname).append(zip);
     return $('#signhead').append(sigcontents).fadeIn("slow");
   }
-  else if (sigdata.obfuscated === false){
+  else {
     var sigcontainer = $('<div>',{id:"sigcontainer",class:"sigcontainer"});
     var firstname = $('<div>',{id:"firstname",class:""}).text(sigdata.firstName);
     var lastname = $('<div>',{id:"lastname",class:""}).text(sigdata.lastName);
-    var zip = $('<div>',{id:"zip"}).text(sigdata.zip);
+    var zip = $('<div>',{id:"zip"}).text(sigdata.zip_code);
     var sigcontents =sigcontainer.append(firstname).append(lastname).append(zip);
     return $('#signhead').append(sigcontents).fadeIn("slow");
   }
@@ -68,8 +83,10 @@ window.viz.displaySignature = function(sigdata){
 
 window.viz.orderSignatures = function(){
     var column = $('<div>',{id:"subcol",class:"col-md-2"});
-    for (var i = 0;i<viz.SampleSingatureData.length;i++){
-      viz.displaySignature(viz.SampleSingatureData[i]);
+    var sigdata = viz.signature_data.concat(viz.signature_redacted_data);
+    var sigdata_sorted = sig_data.sort(viz.sigDateCompare)
+    for (var i = 0;i<sigdata_sorted.length;i++){
+      viz.displaySignature(sigdata_sorted[i]);
       if(i%4 ===0 && i > 3){
         $("#signhead >.sigcontainer").wrapAll(column)
       }
@@ -78,8 +95,7 @@ window.viz.orderSignatures = function(){
 }
 
 $( document ).ready( function(){
-  viz.getSignatures();
-  viz.getSingaturesRedacted();
+  $.when(viz.getSignatures()).then(viz.getSingaturesRedacted()).then(viz.displayData());
   // viz.orderSignatures();
   // $('.alert').hide();
 } );
