8ba985b66ff85630d12064224ff44fa8d0626fc2
Zaki Manian <zaki@manian.org>
2013-12-24 21:42:23 -0800

    Move everything inside the displayable conditional

--- js/visuals.js
+++ js/visuals.js
@@ -97,18 +97,16 @@ window.viz.orderSignatures = function(){
     var i = 0;
     var col_id=0;
     while (i < 54){
+      var data_item =sig_data_sorted.pop();
+      if (viz.displayableSig(data_item)){
       if(i%3 === 0){
        col_id +=1;
         var column = $('<div>',{id:"subcol" +(col_id).toString(),class:"col-sm-2"});
       // setTimeout(function(){$("#signhead").append(column)},50*(i+1));
       $("#signhead").append(column);
       }
-      
-      var data_item =sig_data_sorted.pop();
-      
-      if (viz.displayableSig(data_item)) {
-        viz.displaySignature(data_item,col_id,i+1);
-        i++;
+      viz.displaySignature(data_item,col_id,i+1);
+      i++;
       
         // if(i%3 ===0 && i > 2){
         // setTimeout(function(x){$("#signhead >.sigcontainer").wrapAll(column);},50*(i+1));
