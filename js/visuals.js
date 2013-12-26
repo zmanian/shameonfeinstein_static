0117e1d084a461094b470a585c9a86a45a788429
Zaki Manian <zaki@manian.org>
2013-12-26 10:29:37 -0800

    Trying to render all the signatures

--- js/visuals.js
+++ js/visuals.js
@@ -102,13 +102,13 @@ window.viz.orderSignatures = function(){
     var sig_data_sorted = sig_data.sort(viz.sigDateCompare)
     var i = 0;
     var col_id=0;
-    while (i < 54){
+    while (i < sig_data.length()){
       var data_item =sig_data_sorted.pop();
       if (viz.displayableSig(data_item)){
       if(i%3 === 0){
        col_id +=1;
-        // var column = $('<div>',{id:"subcol" +(col_id).toString(),class:"col-sm-2"});
-        // setTimeout(function(){$("#signhead").append(column)},100*(i+1));
+        var column = $('<div>',{id:"subcol" +(col_id).toString(),class:"col-sm-2"});
+        setTimeout(function(){$("#signhead").append(column)},100*(i+1));
       // $("#signhead").append(column);
       }
       viz.displaySignature(data_item,col_id,i+1);
